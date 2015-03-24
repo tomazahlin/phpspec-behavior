3677a3616b36484a5ae56529fa66da07ee022335
Tomaz Ahlin <tahlin@sessiondigital.de>
2015-03-24 15:16:55 +0100

    First commit.

---
+++ src/Behaviour/ObjectBehavior.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace TAhlin\PhpSpec\Behaviour;
+
+use PhpSpec\ObjectBehavior as BaseBehavior;
+
+/**
+ * Class ObjectBehavior can be used as a base class for PhpSpec classes, as it provides additional common functionality
+ */
+class ObjectBehavior extends BaseBehavior
+{
+    /**
+     * {@inheritdoc}
+     * Provides additional inline matchers for PhpSpec
+     * @return array
+     */
+    public function getMatchers()
+    {
+        return [
+            'haveKey' => function(array $subject, $key) {
+                return array_key_exists($key, $subject);
+            },
+            'haveValue' => function(array $subject, $value) {
+                return in_array($value, $subject);
+            },
+            'haveKeyWithValue' => function(array $subject, $key, $value) {
+                if(!array_key_exists($key, $subject)) {
+                    return false;
+                }
+                return $subject[$key] === $value;
+            },
+            'beEmpty' => function($array) {
+                return count($array) === 0;
+            },
+        ];
+    }
+}
